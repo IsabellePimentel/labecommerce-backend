148694853d5b700ed4ece67352936d94f8731edc
isabellepimentel <isabellechavespimentel@gmail.com>
2023-01-23 21:26:21 -0300

    exercicio-feito

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
-import { TUser, TProduct, TPurchase } from "./types";
+import { TUser, TProduct, TPurchase,Category } from "./types";
 
-export const user: TUser[] = [{
+export const users: TUser[] = [{
     id: "01",
     email: "Bananinha@teste.com",
     password: "54321",
@@ -15,12 +15,12 @@ export const products:TProduct[]=[{
     id: "01",
     name: "chocolate",
     price: 30.22,
-    category: "doce"
+    category: Category.FOOD,
 },{
     id: "02",
     name: "Doce de Leite",
     price: 40.23,
-    category: "doce"
+    category: Category.FOOD,
 }
 ]
 
@@ -37,3 +37,72 @@ export const purchases:TPurchase[]=[{
 }
 
 ]
+
+
+export const  createUser = (idUser: string, emailUser: string, passwordUser:string) =>{
+
+    let newUser = {
+        id:idUser,
+        email: emailUser,
+        password: passwordUser
+    }
+    users.push(newUser)
+    return console.log("Cadastro realizado com sucesso")
+}
+
+export const createProduct= (id:string, name: string, price: number, category: Category): string =>{
+    const newProduct = {
+        id: id,
+        name: name,
+        price: price,
+        category: category
+    }
+    products.push(newProduct)
+    return("Produto criado com sucesso")
+}
+
+
+export const getAllProducts = (): TProduct[ ]=> {
+    return products
+}
+
+export const getProductById = (idToSearch: string) : TProduct[] | undefined =>{
+    return  products.filter((product)=>{
+        if(product.id === idToSearch){
+            return product
+        }
+    })
+    
+}
+
+export const queryProductsByName = (q:string) :void => {
+    const query =  products.filter(
+        (product) => {
+          return(product.name.toLowerCase().includes(q.toLowerCase()))
+        }
+      ) 
+
+      console.table(query)
+}
+
+export const createPurchase = (userId: string, productId: string, quantity: number, totalPrice: number) :void => {
+    const newPurchase:TPurchase = {
+        userId,
+        productId,
+        quantity,
+        totalPrice
+    }
+
+    purchases.push(newPurchase)
+
+    console.log("Compra realizada com sucesso!")
+    console.table(purchases)
+}
+
+export const getAllPurchasesFromUserId = (userIdToSearch:string) :TPurchase[]=> {
+    return purchases.filter(
+        (purchase) => {
+          return(purchase.userId.toLowerCase().includes(userIdToSearch.toLowerCase()))
+        }
+      ) 
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,90 @@
-import { user, products, purchases } from "./database";
+import { users, products, purchases } from "./database";
+import { createUser,createProduct,getAllProducts, getProductById } from "./database";
+import { TProduct, TPurchase, TUser } from "./types";
+import express, { Request, Response } from "express";
 
-console.log(user)
-console.log("Usuario")
 
-console.log(products)
-console.log("Produto")
+const app = express()
 
-console.log(purchases)
-console.log("Compras")
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log("Servidor rodando localhost 3003")
+})
+
+
+app.get('/ping' ,(req: Request, res: Response)=>{
+
+    res.send('Pong!')
+    })
+
+    
+    app.get('/users', (req: Request, res: Response)=>{
+        res.status(200).send(users)
+
+    })
+
+
+    app.post('/users', (req: Request, res: Response) =>{
+        const {id, email, password}= req.body as TUser
+
+        const newuser ={
+            id,
+            email,
+            password
+        }
+
+        users.push(newuser)
+        res.status(201).send("Cliente cadastrado com sucesso!")
+    })
+
+
+    
+
+    app.get('/products', (req: Request, res:Response) =>{
+        res.status(200).send(products)
+    })
+
+
+
+      app.post('/products', (req: Request, res: Response) =>{
+        const {id, name, price, category}= req.body as TProduct
+
+        const newProduct ={
+            id,
+            name,
+            price,
+            category
+        }
+
+        products.push(newProduct)
+        res.status(201).send("Produto cadastrado com sucesso!")
+    })
+
+
+    
+
+    app.get('/products/search', (req: Request, res:Response) =>{
+        const q = req.query.q as string
+
+        const result = products.filter((product)=>{
+            return product.name.toLowerCase().includes(q.toLowerCase())
+        })
+        res.status(200).send(result)
+    })
+
+
+    app.post('/purchases', (req: Request, res: Response) =>{
+        const {userId,productId,quantity,totalPrice}= req.body as TPurchase
+
+        const newpurchases ={
+            userId,
+            productId,
+            quantity,
+            totalPrice
+        }
+
+        purchases.push(newpurchases)
+        res.status(201).send("Compra cadastrada com sucesso!")
+    })
