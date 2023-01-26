86f9dfa13654573be2d056b61642c159925f22ab
isabellepimentel <isabellechavespimentel@gmail.com>
2023-01-25 22:44:35 -0300

    exercicio-feito

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -31,3 +31,45 @@ VALUES ('01', 'Bolo de chocolate', 30, 'Comida'),
 
 
 SELECT * FROM products;
+
+
+SELECT * FROM users;
+
+SELECT * FROM products;
+
+SELECT * FROM products
+WHERE name='Bolo de cenoura';
+
+INSERT INTO users VALUES ('04','joana@gmail.com','D12354');
+
+INSERT INTO products VALUES ('06', 'Luva',20, 'Roupa');
+
+SELECT * FROM products
+WHERE id='04';
+
+DELETE FROM users
+WHERE id='04';
+
+DELETE FROM products
+WHERE id='02';
+
+UPDATE users SET password='e123456' WHERE id='01';
+
+UPDATE products SET price='150' WHERE id='05';
+
+
+SELECT * FROM users
+ORDER BY email ASC;
+
+SELECT * FROM products;
+
+SELECT * FROM products
+ORDER BY price ASC
+limit 0, 20
+;
+
+
+SELECT * FROM products
+WHERE price BETWEEN 20 AND 30
+ORDER BY price ASC;
+
